b7ba6157be80597292343b4e32a53db958e84501
Teagan Lee <teagan.lee.2020@student.lhschools.org>
2020-02-25 12:48:01 -0500

    IntelliJ Recepit

---
+++ src/com/LickingHeights/Main.java
@@ -0,0 +1,35 @@
+package com.LickingHeights;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println ("+---------------------------------+");
+        System.out.println ("|        DANS PUMP AND GO         |");
+        System.out.println ("|         15238 MAIN ST           |");
+        System.out.println ("|            NEWTON               |");
+        System.out.println ("|       CALIFORNIA 93842          |");
+        System.out.println ("|                                 |");
+        System.out.println ("|ST-TX: 11089984    (555) 708-2224|");
+        System.out.println ("|201-02-25                   07:09|");
+        System.out.println ("|TERMINAL: 509338300  OPER: A     |");
+        System.out.println ("|                                 |");
+        System.out.println ("|fuel        [G]  ($/G)       ($) |");
+        System.out.println ("|Pump 9                           |");
+        System.out.println ("|Premium   40.000  1.345     53.80|");
+        System.out.println ("|                                 |");
+        System.out.println ("|Total Owed                  53.80|");
+        System.out.println ("|TOTAL PAID                       |");
+        System.out.println ("|CREDIT CARD                 53.80|");
+        System.out.println ("|                                 |");
+        System.out.println ("|VISA    ************4597         |");
+        System.out.println ("|INV. 972821    AUTH.  54563      |");
+        System.out.println ("|Purchase                         |");
+        System.out.println ("|S 0010010010 00 127              |");
+        System.out.println ("|00 APPROVED  - THANK YOU         |");
+        System.out.println ("|                                 |");
+        System.out.println ("|         --IMPORTANT--           |");
+        System.out.println ("|Retain This Copy For Your Records|");
+        System.out.println ("+---------------------------------+");
+
+    }
+}
